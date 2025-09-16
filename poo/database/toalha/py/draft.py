b72a27af04eddd1ca63bc2dd606b0df4269e0aea
Carlos Augusto <168469953+gutoteix@users.noreply.github.com>
2025-09-16 20:24:24 +0000

    tarefa toalha feita

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,1 +1,17 @@
-print("minha toalha é a que tiver no banheiro")
+class Towel:
+    def __init__(self, color: str, size: str):
+        self.color = "red"
+        self.size = "p"
+        self.wetness = 0
+    
+    def __str__(self):
+        return f"color:{self.color}, tam:{self.size}, hum:{self.wetness}"
+
+towel = Towel("green", "G")
+towel2 = Towel("black", "GG")
+
+
+towel.color = "White"
+print(towel.color)
+print(towel.size)
+print(towel.wetness)
